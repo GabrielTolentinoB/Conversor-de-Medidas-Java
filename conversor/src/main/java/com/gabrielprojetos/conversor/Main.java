da17bb3bcb7c709294bd7fb1d092c79925671eb5
Gabriel <gabrieltolentinoberloffa@outlook.com>
2026-09-24 20:08:26 +0000

    Salvando projeto para continuar posteriormente

--- conversor/src/main/java/com/gabrielprojetos/conversor/Main.java
+++ conversor/src/main/java/com/gabrielprojetos/conversor/Main.java
@@ -1,10 +1,46 @@
 package com.gabrielprojetos.conversor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("");
+        System.out.println("");
+	    System.out.println("seja bem vindo ao conversor.");
+		System.out.println("Digite o número da operação.");
+        System.out.println("");
+		System.out.println("1 - Comprimento");
+		System.out.println("2 - Massa");
+		System.out.println("3 - Temperatura");
+		System.out.println("4 - Volume");
+		System.out.println("5 - Tempo");
+		System.out.println("6 - Sair");
 
+        int choice;
+        try (Scanner scanner = new Scanner(System.in)) {
+
+        do 
         
-        System.out.println("Hello world!");
+        {
+            choice = scanner.nextInt();
+
+            if (choice == (1)) {
+
+
+
+
+
+
+            } else if (choice == (2)) 
+                {
+                
+            }
+
+
+
+
+        } while(choice != 6);
+        }
     }
 }
