0efe53a144400078c7101934243382ea7fa35293
Gabriel <gabrieltolentinoberloffa@outlook.com>
2026-09-25 17:20:44 -0300

    Complementando ainda mais o projeto, para posteriormente colocar métodos

--- conversor/src/main/java/com/gabrielprojetos/conversor/Main.java
+++ conversor/src/main/java/com/gabrielprojetos/conversor/Main.java
@@ -8,39 +8,129 @@ public class Main {
         System.out.println("");
         System.out.println("");
 	    System.out.println("seja bem vindo ao conversor.");
-		System.out.println("Digite o número da operação.");
-        System.out.println("");
-		System.out.println("1 - Comprimento");
-		System.out.println("2 - Massa");
-		System.out.println("3 - Temperatura");
-		System.out.println("4 - Volume");
-		System.out.println("5 - Tempo");
-		System.out.println("6 - Sair");
+
 
         int choice;
+		int choiceIn;
+
+		
+		
         try (Scanner scanner = new Scanner(System.in)) {
 
         do 
         
         {
+			System.out.println("Digite o número da operação.");
+        	System.out.println("");
+			System.out.println("1 - Comprimento");
+			System.out.println("2 - Massa");
+			System.out.println("3 - Temperatura");
+			System.out.println("4 - Tempo");
+			System.out.println("5 - Sair");
+			
             choice = scanner.nextInt();
+			scanner.readLine();
 
-            if (choice == (1)) {
+            if (choice == 1) {
+				System.out.println("Qual das operações deseja realizar?");
+				System.out.println("1 - Centimetros para Polegadas");
+				System.out.println("2 - Polegadas para Centimetros");
+				System.out.println("3 - Kilômetros para Milhas");
+				System.out.println("4 - Milhas para Kilômetros");
+				System.out.println("5 - Sair");
+
+				do {
+					
+					choiceIn = scanner.readInt();
+					scanner.readLine();
+
+					if (choiceIn == 1) {
+						System.out.println("");
+						System.out.println("Opção 1 Selecionada");
+						
+						System.out.println("Digite o valor dos centímetros");
+						float n1 = scanner.nextFloat();
+						scanner.nextLine();
+						
+						System.out.println();
+						System.out.println("O valor " + n1 + " en polegadas é:" + (n1 * 2,54));
+
+					} else if (choiceIn == 2) {
+					
+						System.out.println("");
+						System.out.println("Opção 2 Selecionada");
+						
+						System.out.println("Digite o valor das polegadas");
+						float n1 = scanner.nextFloat();
+						scanner.nextLine();
+						
+						System.out.println();
+						System.out.println("O valor " + n1 + " en polegadas é:" + (n1 / 2,54));
+					
+					} else if (choiceIn == 3) {
+
+						System.out.println("");
+						System.out.println("Opção 3 Selecionada");
+						
+						System.out.println("Digite o valor dos kilômetros");
+						float n1 = scanner.nextFloat();
+						scanner.nextLine();
+						
+						System.out.println();
+						System.out.println("O valor " + n1 + " en polegadas é:" + (n1 / 0,621371));
+					
+					} else if (choiceIn == 4) {
+
+						System.out.println("");
+						System.out.println("Opção 4 Selecionada");
+						
+						System.out.println("Digite o valor das milhas");
+						float n1 = scanner.nextFloat();
+						scanner.nextLine();
+						
+						System.out.println();
+						System.out.println("O valor " + n1 + " en polegadas é:" + (n1 * 0,621371));
+
+					} else if (choiseIn == 5) {
+						break;
+            		} else {System.out.println("Digite um número válido");} 
+
+				} while (true);
 
 
 
 
-
-
-            } else if (choice == (2)) 
-                {
+					
+						
+			} else if (choice == 2) {
+				System.out.println("Qual das operações deseja realizar?");
+				System.out.println("1 - Kilos para Libras");
+				System.out.println("2 - Libras para Kilos");
+				choiceIn = scanner.readInt();
+				scanner.readLine();                
                 
-            }
+            	
+				
+			} else if (choice == 3) {
+            	System.out.println("Qual das operações deseja realizar?");
+				System.out.println("1 - Celcius para Farenhaitz");
+				System.out.println("2 - Farenhaitz para Celcius");
+				choiceIn = scanner.readInt();
+				scanner.readLine();       
+								
+			} else if (choice == 4) {
+            	System.out.println("Qual das operações deseja realizar?");
+				System.out.println("1 - Minutos para Horas");
+				System.out.println("2 - Horas para Minutos");
+				choiceIn = scanner.readInt();
+				scanner.readLine();            	
 
-
-
-
-        } while(choice != 6);
-        }
+			} else if (choice == 5) {
+				break;
+            } else {System.out.println("Digite um número válido");} 
+			
+        } while(true);
+			
+	System.out.println("Saindo...");			
     }
 }
